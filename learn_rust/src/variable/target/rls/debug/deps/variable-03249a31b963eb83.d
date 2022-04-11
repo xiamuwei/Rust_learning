12668a78cd3454cd1231dc4d3e19b2d243cbcc46
root <2827210233@qq.com>
2022-04-11 20:07:08 +0800

    rust code

---
+++ learn_rust/src/variable/target/rls/debug/deps/variable-03249a31b963eb83.d
@@ -0,0 +1,5 @@
+d:\gorepository\Rust_learning\learn_rust\src\variable\target\rls\debug\deps\variable-03249a31b963eb83.rmeta: src\main.rs
+
+d:\gorepository\Rust_learning\learn_rust\src\variable\target\rls\debug\deps\variable-03249a31b963eb83.d: src\main.rs
+
+src\main.rs:
---
+++ learn_rust/src/variable/target/rls/debug/deps/variable-35eadb8fbd9bf688.d
@@ -0,0 +1,5 @@
+d:\gorepository\Rust_learning\learn_rust\src\variable\target\rls\debug\deps\variable-35eadb8fbd9bf688.rmeta: src\main.rs
+
+d:\gorepository\Rust_learning\learn_rust\src\variable\target\rls\debug\deps\variable-35eadb8fbd9bf688.d: src\main.rs
+
+src\main.rs:
